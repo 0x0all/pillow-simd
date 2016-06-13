3ba029d09e0eb76a547190e5bc889e72ffbb04c0
homm <homm86@gmail.com>
2016-06-13 18:25:54 +0300

    4x1 horizontal avx2 pass

--- libImaging/ResampleSIMDHorizontalConv.c
+++ libImaging/ResampleSIMDHorizontalConv.c
@@ -15,7 +15,7 @@ ImagingResampleHorizontalConvolution8u(UINT32 *lineOut, UINT32 *lineIn,
 
 #if defined(__AVX2__)
 
-        __m256i sss256 = _mm256_set1_epi32((1 << (coefs_precision -1)) + xmax / 2);
+        __m256i sss256 = _mm256_set1_epi32((1 << (coefs_precision -2)) + xmax / 4);
 
         for (; x < xmax - 7; x += 8) {
             __m256i pix, mmk, source;
@@ -39,6 +39,25 @@ ImagingResampleHorizontalConvolution8u(UINT32 *lineOut, UINT32 *lineIn,
             mmk = _mm256_shuffle_epi8(ksource, _mm256_set_epi8(
                 15,14, 13,12, 15,14, 13,12, 15,14, 13,12, 15,14, 13,12,
                 7,6, 5,4, 7,6, 5,4, 7,6, 5,4, 7,6, 5,4));
+            sss256 = _mm256_add_epi32(sss256, _mm256_madd_epi16(pix, mmk));
+        }
+
+        for (; x < xmax - 3; x += 4) {
+            __m256i pix, mmk, source;
+            __m128i tmp = _mm_cvtsi64_si128(*(int64_t *) &k[x]);
+            __m256i ksource = _mm256_insertf128_si256(
+                _mm256_castsi128_si256(tmp), tmp, 1);
+
+            tmp = _mm_loadu_si128((__m128i *) &lineIn[x + xmin]);
+            source = _mm256_insertf128_si256(
+                _mm256_castsi128_si256(tmp), tmp, 1);
+
+            pix = _mm256_shuffle_epi8(source, _mm256_set_epi8(
+                -1,15, -1,11, -1,14, -1,10, -1,13, -1,9, -1,12, -1,8,
+                -1,7, -1,3, -1,6, -1,2, -1,5, -1,1, -1,4, -1,0));
+            mmk = _mm256_shuffle_epi8(ksource, _mm256_set_epi8(
+                7,6, 5,4, 7,6, 5,4, 7,6, 5,4, 7,6, 5,4, 
+                3,2, 1,0, 3,2, 1,0, 3,2, 1,0, 3,2, 1,0));
             sss256 = _mm256_add_epi32(sss256, _mm256_madd_epi16(pix, mmk));
         }
 
@@ -102,6 +121,8 @@ ImagingResampleHorizontalConvolution8u(UINT32 *lineOut, UINT32 *lineIn,
             sss = _mm_add_epi32(sss, _mm_madd_epi16(pix, mmk));
         }
 
+#endif
+
         for (; x < xmax - 1; x += 2) {
             __m128i pix, mmk;
             __m128i source = _mm_cvtsi64_si128(*(int64_t *) &lineIn[x + xmin]);
@@ -113,8 +134,6 @@ ImagingResampleHorizontalConvolution8u(UINT32 *lineOut, UINT32 *lineIn,
                 3,2, 1,0, 3,2, 1,0, 3,2, 1,0, 3,2, 1,0));
             sss = _mm_add_epi32(sss, _mm_madd_epi16(pix, mmk));
         }
-
-#endif
 
         for (; x < xmax; x ++) {
             __m128i pix = _mm_cvtepu8_epi32(*(__m128i *) &lineIn[x + xmin]);
