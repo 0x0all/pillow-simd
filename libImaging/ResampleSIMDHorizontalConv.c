78d2208fdc9d5ad20de46eeab105b8f9c4d4ff9b
homm <homm86@gmail.com>
2016-06-13 18:13:30 +0300

    horizontal 4x1 avx2 pass

--- libImaging/ResampleSIMDHorizontalConv.c
+++ libImaging/ResampleSIMDHorizontalConv.c
@@ -13,32 +13,41 @@ ImagingResampleHorizontalConvolution8u(UINT32 *lineOut, UINT32 *lineIn,
         k = &kk[xx * kmax];
         x = 0;
 
-// #if defined(__AVX2__)
+#if defined(__AVX2__)
 
-//         __m256i sss256 = _mm256_set1_epi32((1 << (coefs_precision - 9 -2)) + xmax / 4);
+        __m256i sss256 = _mm256_set1_epi32((1 << (coefs_precision -1)) + xmax / 2);
 
-//         for (; x < xmax - 3; x += 4) {
-//             __m256i pix, mmk, mul;
-//             __m256i source = _mm256_castsi128_si256(
-//                 _mm_loadu_si128((__m128i *) &lineIn[x + xmin]));
-//             __m256i ksource = _mm256_set1_epi64x(*(int64_t *) &k[x]);
-//             source = _mm256_unpacklo_epi64(source, source);
+        for (; x < xmax - 7; x += 8) {
+            __m256i pix, mmk, source;
+            __m128i tmp = _mm_loadu_si128((__m128i *) &k[x]);
+            __m256i ksource = _mm256_insertf128_si256(
+                _mm256_castsi128_si256(tmp), tmp, 1);
 
-//             pix = _mm256_unpacklo_epi8(source, _mm256_setzero_si256());
-//             mmk = _mm256_shuffle_epi8(ksource, _mm256_set_epi8(
-//                 3,2, 3,2, 3,2, 3,2, 1,0, 1,0, 1,0, 1,0,
-//                 3,2, 3,2, 3,2, 3,2, 1,0, 1,0, 1,0, 1,0));
-//             mul = _mm256_mulhi_epi16(_mm256_slli_epi16(pix, 7), mmk);
-//             sss256 = _mm256_add_epi32(sss256, _mm256_cvtepi16_epi32(mul));
-//             sss256 = _mm256_add_epi32(sss256, _mm256_cvtepi16_epi32(_mm256_srli_si128(mul, 8)));
-//         }
+            source = _mm256_loadu_si256((__m256i *) &lineIn[x + xmin]);
 
-//         sss = _mm_add_epi32(
-//             _mm256_extractf128_si256(sss256, 0),
-//             _mm256_extractf128_si256(sss256, 1)
-//         );
+            pix = _mm256_shuffle_epi8(source, _mm256_set_epi8(
+                -1,7, -1,3, -1,6, -1,2, -1,5, -1,1, -1,4, -1,0,
+                -1,7, -1,3, -1,6, -1,2, -1,5, -1,1, -1,4, -1,0));
+            mmk = _mm256_shuffle_epi8(ksource, _mm256_set_epi8(
+                11,10, 9,8, 11,10, 9,8, 11,10, 9,8, 11,10, 9,8,
+                3,2, 1,0, 3,2, 1,0, 3,2, 1,0, 3,2, 1,0));
+            sss256 = _mm256_add_epi32(sss256, _mm256_madd_epi16(pix, mmk));
 
-// #else
+            pix = _mm256_shuffle_epi8(source, _mm256_set_epi8(
+                -1,15, -1,11, -1,14, -1,10, -1,13, -1,9, -1,12, -1,8,
+                -1,15, -1,11, -1,14, -1,10, -1,13, -1,9, -1,12, -1,8));
+            mmk = _mm256_shuffle_epi8(ksource, _mm256_set_epi8(
+                15,14, 13,12, 15,14, 13,12, 15,14, 13,12, 15,14, 13,12,
+                7,6, 5,4, 7,6, 5,4, 7,6, 5,4, 7,6, 5,4));
+            sss256 = _mm256_add_epi32(sss256, _mm256_madd_epi16(pix, mmk));
+        }
+
+        sss = _mm_add_epi32(
+            _mm256_extractf128_si256(sss256, 0),
+            _mm256_extractf128_si256(sss256, 1)
+        );
+
+#else
 
         sss = _mm_set1_epi32((1 << (coefs_precision -1)) + xmax / 2);
 
@@ -105,7 +114,7 @@ ImagingResampleHorizontalConvolution8u(UINT32 *lineOut, UINT32 *lineIn,
             sss = _mm_add_epi32(sss, _mm_madd_epi16(pix, mmk));
         }
 
-// #endif
+#endif
 
         for (; x < xmax; x ++) {
             __m128i pix = _mm_cvtepu8_epi32(*(__m128i *) &lineIn[x + xmin]);
